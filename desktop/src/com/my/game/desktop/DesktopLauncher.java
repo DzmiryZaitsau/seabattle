eff07baf135e550e38b5b3ff1948c038b2cfeb40
DzmitryZaitsau <dimlepus@gmail.com>
2020-07-28 11:21:54 +0300

    init

---
+++ desktop/src/com/my/game/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.my.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.my.game.SeaBattle;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "SeaBattle";
+		config.width = 800;
+		config.height = 480;
+		new LwjglApplication(new SeaBattle(), config);
+	}
+}
